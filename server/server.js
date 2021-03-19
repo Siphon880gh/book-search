69263a1aeb312bda39236f4451dea7763b09efe4
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-18 17:56:12 -0700

    Implementing GraphQL: Server.js with breaking errors until other parts of Apollo Server are implemented

--- server/server.js
+++ server/server.js
@@ -1,21 +1,42 @@
+// Modules
 const express = require('express');
+// const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express'); // Apollo has server and client libraries
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+// Server dependencies
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+
+// Server
 const PORT = process.env.PORT || 3001;
+const app = express();
+// - Apollo server library
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context: authMiddleware // TODO: When Auth
+});
+
+// server.applyMiddleware({ app }); // TODO: When Auth
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve client/build as static assets if we're in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Other URIs serve the frontend homepage
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
 });
