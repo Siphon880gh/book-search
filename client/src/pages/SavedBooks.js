130519b28e9f879893a149bb819509cf299df72b
Weng Fei Fung <weffung@ucdavis.edu>
2021-03-19 12:15:50 -0700

    Refactored: Removed controllers, routers, API.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 // import { getMe } from '../utils/API';
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/google-api';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 // Apollo React Mutators
